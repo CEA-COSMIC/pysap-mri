65575fb90905c4ed54cc7420a617cf040d891ce4
Antoine Grigis <antoine.grigis@cea.fr>
2019-03-19 09:03:00 +0100

    Init commit.

---
+++ README.rst
@@ -0,0 +1,16 @@
+
+pySAP-mri
+===============
+
+Python Sparse data Analysis Package external MRI plugin.
+
+This work is made available by a community of people, amoung which the
+CEA Neurospin UNATI and CEA CosmoStat laboratories, in particular A. Grigis,
+J.-L. Starck, P. Ciuciu, and S. Farrens.
+
+
+Important links
+===============
+
+- Official pySAP source code repo: https://github.com/cea-cosmic/pysap
+- pySAP HTML documentation (last stable release): http://cea-cosmic.github.io/pysap
---
+++ setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+##########################################################################
+# pySAP - Copyright (C) CEA, 2018
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+# System import
+from __future__ import print_function
+import os
+from setuptools import setup, find_packages
+
+
+# Global parameters
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering"]
+AUTHOR = """
+Antoine Grigis <antoine.grigis@cea.fr>
+Samuel Farrens <samuel.farrens@gmail.com>
+Jean-Luc Starck <jl.stark@cea.fr>
+Philippe Ciuciu <philippe.ciuciu@cea.fr>
+"""
+
+# Write setup
+setup(
+    name="pysap-mri",
+    description="Python Sparse data Analysis Package external MRI plugin.",
+    long_description="Python Sparse data Analysis Package external MRI plugin.",
+    license="CeCILL-B",
+    classifiers="CLASSIFIERS",
+    author=AUTHOR,
+    author_email="XXX",
+    version="0.0.0",
+    url="https://github.com/CEA-COSMIC/pysap-mri",
+    packages=find_packages(),
+    platforms="OS Independent"
+)
