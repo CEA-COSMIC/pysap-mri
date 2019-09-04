0cc5c4322a02fdfd234efda1d12956e65b0ac819
chaithyagr <chaithyagr@gmail.com>
2019-09-04 17:44:47 +0200

    Added Example Codes

---
+++ examples/p_mri_3d_recon.py
@@ -0,0 +1,119 @@
+"""
+Neuroimaging cartesian reconstruction
+=====================================
+
+Credit: Chaithya G R
+
+In this tutorial we will reconstruct a 3D-MRI image from the sparse kspace
+measurments.
+"""
+
+# Package import
+import pysap
+from modopt.opt.cost import costObj
+from mri.reconstruct.fourier import NFFT
+from mri.parallel_mri.gradient import Gradient_pMRI
+from mri.numerics.linear import WaveletN
+from mri.numerics.proximity import Threshold
+from mri.parallel_mri.extract_sensitivity_maps import extract_k_space_center_and_locations, get_Smaps, get_3D_smaps
+from mri.numerics.reconstruct import sparse_rec_fista
+
+# Third party import
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+import twixreader
+
+def get_raw_data(filename):
+    # Function that reads a SIEMENS .dat file and returns a k space data
+    file = twixreader.read_twix(filename)
+    measure = file.read_measurement(1)
+    buffer = measure.get_meas_buffer(0)
+    data = np.asarray(buffer[:])
+    data = np.swapaxes(data,1,2)
+    data = np.reshape(data, (data.shape[0]*data.shape[1], data.shape[2]))
+    return data.T
+
+def get_samples(filename):
+    sample_locations = loadmat(filename)['samples']
+    norm_factor = 2*np.max(np.abs(sample_locations), axis=0)
+    sample_locations = sample_locations / norm_factor
+    return sample_locations
+
+# Loading input data
+N = 64
+Nz = 30
+kspace_loc=[]
+kspace_data=[]
+try:
+    (kspace_loc, kspace_data) = np.load("/neurospin/optimed/Chaithya/Temp_data.npy")
+except:
+    print("Could not find temp file, loading data!")
+    image_name = '/neurospin/optimed/Chaithya/20190802_benchmark_3D_v5/raw/' \
+            'meas_MID00457_FID13121_nsCSGRE3D_N384_FOV192_Nz64_nc32_ns2049_OS2.dat'
+    mask_name = '/neurospin/optimed/Chaithya/20190802_benchmark_3D_v5/samples_9.mat'
+    kspace_data = get_raw_data(image_name)
+    kspace_loc = get_samples(mask_name)
+    kspace_loc = kspace_loc[::100,:]
+    kspace_data = kspace_data[:,::100]
+    np.save("/neurospin/optimed/Chaithya/Temp_data.npy", (kspace_loc, kspace_data))
+
+try:
+    Smaps = np.load("/neurospin/optimed/Chaithya/Temp_data2.npy")
+except:
+    data_thresholded, samples_thresholded = extract_k_space_center_and_locations(
+        data_values=kspace_data,
+        samples_locations=kspace_loc,
+        thr=(0.05, 0.05, 0.05),
+        img_shape=(N, N, Nz))
+    Smaps, SOS_Smaps = get_Smaps(
+        k_space=data_thresholded,
+        img_shape=(N, N, Nz),
+        samples=samples_thresholded,
+        mode='NFFT',
+        min_samples=np.min(samples_thresholded, axis=0),
+        max_samples=np.max(samples_thresholded, axis=0), n_cpu=1)
+    np.save("/neurospin/optimed/Chaithya/Temp_data2.npy", Smaps)
+
+
+max_iter = 150
+data = np.zeros((N, N, Nz))
+linear_op = WaveletN(wavelet_name="BiOrthogonalTransform3D",
+                     nb_scale=4, dim=3)
+
+coeff = linear_op.op(data)
+
+print(coeff)
+
+fourier_op = NFFT(samples=kspace_loc, shape=(N, N, Nz))
+
+gradient_op_cd = Gradient_pMRI(data=kspace_data,
+                               fourier_op=fourier_op,
+                               linear_op=linear_op, S=Smaps)
+
+mu_value = 1e-5
+beta = 1e-15
+prox_op = Threshold(None)
+#############################################################################
+# FISTA optimization
+# ------------------
+#
+# We now want to refine the zero order solution using a FISTA optimization.
+# Here no cost function is set, and the optimization will reach the
+# maximum number of iterations. Fill free to play with this parameter.
+
+# Start the FISTA reconstruction
+x_final, y_final, cost, metrics = sparse_rec_fista(
+    gradient_op=gradient_op_cd,
+    linear_op=linear_op,
+    prox_op=prox_op,
+    cost_op=costObj([gradient_op_cd, prox_op]),
+    lambda_init=0.0,
+    max_nb_of_iter=max_iter,
+    atol=0e-4,
+    verbose=0)
+image_rec = pysap.Image(data=np.sqrt(np.sum(np.abs(x_final)**1, axis=0)))
+image_rec.show()
+plt.plot(cost)
+plt.show()
+plt.imsave("OSCAR_Undecimated_filter_SRF.png", image_rec)
---
+++ examples/pmri_3d_example.npy.py
@@ -0,0 +1,190 @@
+"""
+Neuroimaging cartesian reconstruction
+=====================================
+
+Credit: L. El Gueddari
+
+In this tutorial we will reconstruct an MRI image from the sparse kspace
+measurments.
+
+"""
+
+# Package import
+from pysap.data import get_sample_data
+from mri.numerics.proximity import Threshold
+from mri.numerics.gradient import Gradient_pMRI
+from mri.numerics.reconstruct import sparse_rec_fista
+from mri.numerics.reconstruct import sparse_rec_condatvu
+from mri.reconstruct.fourier import NFFT
+from mri.reconstruct.utils import imshow3D
+from mri.parallel_mri.cost import GenericCost
+from mri.reconstruct_3D.linear import pyWavelet3
+from mri.reconstruct.utils import normalize_frequency_locations
+from mri.reconstruct_3D.extract_sensitivity_maps import (
+    extract_k_space_center,
+    get_3D_smaps)
+
+# Third party import
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Loading input data
+Il = get_sample_data("3d-pmri")
+Iref = np.squeeze(np.sqrt(np.sum(np.abs(Il)**2, axis=0)))
+Smaps = np.asarray([Il[channel]/Iref for channel in range(Il.shape[0])])
+
+imshow3D(Iref, display=True)
+
+samples = get_sample_data("mri-radial-3d-samples").data
+
+samples = normalize_frequency_locations(samples)
+#############################################################################
+# Generate the kspace
+# -------------------
+#
+# From the 2D brain slice and the acquistion mask, we generate the acquisition
+# measurments, the observed kspace.
+# We then reconstruct the zero order solution.
+
+# Generate the subsampled kspace
+
+gen_fourier_op = NFFT3(samples=samples,
+                       shape=(128, 128, 160))
+
+print('Generate the k-space')
+
+kspace_data = np.asarray([gen_fourier_op.op(Il[channel]) for channel
+                          in range(Il.shape[0])])
+
+print("K-space locations  shape", samples.shape)
+
+samples_min = [np.min(samples[:, idx]) for idx in range(samples.shape[1])]
+samples_max = [np.max(samples[:, idx]) for idx in range(samples.shape[1])]
+
+print('After normalization, k-space samples min', samples_min)
+print('After normalization, k-space samples max', samples_max)
+
+samples_center, kspace_center = extract_k_space_center(
+    data_value=kspace_data,
+    samples_locations=samples,
+    thr=(0.1, 0.1, 0.1),
+    img_shape=Iref.shape)
+
+print("Center of k-space extracted above the threshold ", kspace_center.shape)
+print("Center of k-space locations extracted above the threshold ",
+      samples_center.shape)
+
+Smaps, I_SOS = get_3D_smaps(
+    k_space=kspace_center,
+    img_shape=Iref.shape,
+    samples=samples_center,
+    mode='gridding',
+    samples_min=samples_min,
+    samples_max=samples_max)
+
+print("Smaps' shape ", Smaps.shape)
+
+imshow3D(np.abs(I_SOS), display=True)
+
+#############################################################################
+# FISTA optimization
+# ------------------
+#
+# We now want to refine the zero order solution using a FISTA optimization.
+# Here no cost function is set, and the optimization will reach the
+# maximum number of iterations. Fill free to play with this parameter.
+
+# Start the FISTA reconstruction
+max_iter = 2
+linear_op = pyWavelet3(wavelet_name="sym4",
+                       nb_scale=4)
+
+fourier_op = NFFT3(samples=samples,
+                   shape=Iref.shape)
+
+print('Generate the zero order solution')
+
+rec_0 = np.asarray([fourier_op.adj_op(kspace_data[l]) for l in range(32)])
+imshow3D(np.squeeze(np.sqrt(np.sum(np.abs(rec_0)**2, axis=0))),
+         display=True)
+
+gradient_op = Gradient_pMRI(data=kspace_data,
+                            fourier_op=fourier_op,
+                            linear_op=linear_op,
+                            S=Smaps)
+
+prox_op = Threshold(None)
+
+cost_synthesis = GenericCost(
+    gradient_op=gradient_op,
+    prox_op=prox_op,
+    linear_op=None,
+    initial_cost=1e6,
+    tolerance=1e-4,
+    cost_interval=1,
+    test_range=4,
+    verbose=True,
+    plot_output=None)
+
+
+x_final, transform, cost, metrics = sparse_rec_fista(
+    gradient_op=gradient_op,
+    linear_op=linear_op,
+    prox_op=prox_op,
+    cost_op=cost_synthesis,
+    mu=0,
+    lambda_init=1.0,
+    max_nb_of_iter=max_iter,
+    atol=1e-4,
+    verbose=1)
+imshow3D(np.abs(x_final), display=True)
+
+
+plt.figure()
+plt.plot(cost)
+plt.show()
+#############################################################################
+# Condata-Vu optimization
+# -----------------------
+#
+# We now want to refine the zero order solution using a Condata-Vu
+# optimization.
+# Here no cost function is set, and the optimization will reach the
+# maximum number of iterations. Fill free to play with this parameter.
+
+# Start the CONDAT-VU reconstruction
+max_iter = 1
+gradient_op_cd = Gradient_pMRI(data=kspace_data,
+                               fourier_op=fourier_op,
+                               S=Smaps)
+
+cost_analysis = GenericCost(
+    gradient_op=gradient_op_cd,
+    prox_op=prox_op,
+    linear_op=linear_op,
+    initial_cost=1e6,
+    tolerance=1e-4,
+    cost_interval=1,
+    test_range=4,
+    verbose=True,
+    plot_output=None)
+
+x_final, transform = sparse_rec_condatvu(
+    gradient_op=gradient_op_cd,
+    linear_op=linear_op,
+    prox_dual_op=prox_op,
+    cost_op=cost_analysis,
+    std_est=None,
+    std_est_method="dual",
+    std_thr=2.,
+    mu=0,
+    tau=None,
+    sigma=None,
+    relaxation_factor=1.0,
+    nb_of_reweights=0,
+    max_nb_of_iter=max_iter,
+    add_positivity=False,
+    atol=1e-4,
+    verbose=1)
+
+imshow3D(np.abs(x_final), display=True)
