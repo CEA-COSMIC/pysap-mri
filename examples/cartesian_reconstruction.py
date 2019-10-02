f953f6a40668733902e776b3db5d2a3f68e815e5
chaithyagr <chaithyagr@gmail.com>
2019-10-02 14:49:38 +0200

    Updates to examples. Working Non Cartesian

--- examples/cartesian_reconstruction.py
+++ examples/cartesian_reconstruction.py
@@ -23,30 +23,45 @@ from mri.numerics.fourier import FFT2
 from mri.numerics.utils import generate_operators
 from mri.numerics.utils import convert_mask_to_locations, \
     convert_locations_to_mask
+from modopt.math.metrics import ssim
 
 # Third party import
 import numpy as np
-from scipy.io import loadmat
+
+
+def generate_cartesian_mask(img_size, acceleration_factor=3, decay=1, save_filename=None):
+    kspace_lines = np.linspace(-1/2., 1/2., img_size) * img_size
+    # Define the sampling density
+    p_decay = np.power(np.abs(kspace_lines),-decay)
+    p_decay = p_decay/np.sum(p_decay)
+    cdf_pdecay = np.cumsum(p_decay)
+    # generate its CDF
+    nb_samples = (int)(img_size/acceleration_factor)
+    samples = np.random.uniform(0, 1, nb_samples)
+    gen_klines = [int(kspace_lines[np.argwhere(cdf_pdecay == min(cdf_pdecay[(cdf_pdecay - r) > 0]))])
+                  for r in samples]
+    gen_klines_int = ((np.array(gen_klines) - 1) / 1).astype(int) + (int)(img_size/2)
+    sampled_klines = np.array(np.unique(gen_klines_int))
+    kspace_mask = np.zeros((img_size,img_size), dtype="float64")
+    nblines = np.size(sampled_klines)
+    kspace_mask[sampled_klines, :] = np.ones((nblines,img_size) , dtype="float64")
+    mask = pysap.Image(data=kspace_mask)
+    if save_filename is not None:
+        pysap.utils.save_image(mask, save_filename)
+    return mask
 
 # Loading input data
-image_name = '../../../Data/meas_MID41_CSGRE_ref_OS1_FID14687.mat'
-k_space_ref = loadmat(image_name)['ref']
-k_space_ref /= np.linalg.norm(k_space_ref)
-Sl = np.zeros((32, 512, 512), dtype='complex128')
-for channel in range(k_space_ref.shape[-1]):
-    Sl[channel] = np.fft.fftshift(np.fft.ifft2(np.reshape(
-            k_space_ref[:, channel], (512, 512))))
-image = pysap.Image(data=np.sqrt(np.sum(np.abs(Sl)**2, 0)))
+image = pysap.utils.load_image('../../../Data/Pysap_examples/base_image.npy')
 
-# Add Noise to input MRI Image
-
-# Obtain MRI Mask
-mask = get_sample_data("mri-mask")
+# Obtain K-Space Cartesian Mask
+try:
+    mask = pysap.utils.load_image('../../../Data/Pysap_examples/mask_cartesian.npy')
+except:
+    mask = generate_cartesian_mask(image.shape[0], save_filename='../../../Data/Pysap_examples/mask_cartesian.npy')
 
 # View Input
 image.show()
 mask.show()
-
 
 #############################################################################
 # Generate the kspace
@@ -67,6 +82,10 @@ kspace_data = fourier_op.op(image)
 image_rec0 = pysap.Image(data=fourier_op.adj_op(kspace_data),
                          metadata=image.metadata)
 image_rec0.show()
+pysap.utils.save_image(image_rec0, '../../../Data/Pysap_examples/zero_order_reconstruct.npy')
+
+base_ssim = ssim(image_rec0, image)
+print(base_ssim)
 
 #############################################################################
 # FISTA optimization
@@ -77,12 +96,14 @@ image_rec0.show()
 # maximum number of iterations.
 
 # Generate operators
+my_ssims =[]
+mus = []
 gradient_op, linear_op, prox_op, cost_op = generate_operators(
     data=kspace_data,
     wavelet_name="sym8",
     samples=kspace_loc,
     nb_scales=4,
-    mu=5e-6,
+    mu=1e-06,
     non_cartesian=False,
     uniform_data_shape=None,
     gradient_space="synthesis",
@@ -95,9 +116,12 @@ x_final, transform, costs, metrics = sparse_rec_fista(
     linear_op,
     prox_op,
     cost_op,
-    lambda_init=10,
+    lambda_init=1.0,
     max_nb_of_iter=max_iter,
     atol=1e-4,
     verbose=1)
 image_rec = pysap.Image(data=np.abs(x_final))
 image_rec.show()
+recon_ssim = ssim(image_rec, image)
+print('The Reconstruction SSIM is : ' + str(recon_ssim))
+
--- examples/non_cartesian_reconstruction.py
+++ examples/non_cartesian_reconstruction.py
@@ -2,7 +2,7 @@
 Neuroimaging non-cartesian reconstruction
 =========================================
 
-Credit: L Elgueddari,
+Author: Chaithya G R
 
 In this tutorial we will reconstruct an MRI image from non-cartesian kspace
 measurments.
@@ -19,20 +19,19 @@ We also add some gaussian noise in the image space.
 import pysap
 from pysap.data import get_sample_data
 from mri.numerics.fourier import NFFT, FFT2
-from mri.numerics.reconstruct import sparse_rec_fista, sparse_rec_pogm
+from mri.numerics.reconstruct import sparse_rec_fista
 from mri.numerics.utils import generate_operators
-from mri.numerics.utils import convert_locations_to_mask, convert_mask_to_locations
+from mri.numerics.utils import convert_locations_to_mask, \
+    convert_mask_to_locations
 
 # Third party import
+from modopt.math.metrics import ssim
 import numpy as np
 
-
 # Loading input data
-image = get_sample_data("mri-slice-nifti")
-# Add Noise to input MRI Image
-image.data += np.random.randn(*image.shape) * 20.
+image = pysap.utils.load_image('../../../Data/Pysap_examples/base_image.npy')
 
-# Obtain MRI Mask
+# Obtain MRI non-cartesian mask
 radial_mask = get_sample_data("mri-radial-samples")
 kspace_loc = radial_mask.data
 mask = pysap.Image(data=convert_locations_to_mask(kspace_loc, image.shape))
@@ -54,13 +53,14 @@ fourier_op = NFFT(samples=kspace_loc, shape=image.shape)
 kspace_obs = fourier_op.op(image.data)
 
 # Zero order solution
-image_rec0 = pysap.Image(data=fourier_op.adj_op(kspace_obs))
+mask_gridded = convert_locations_to_mask(kspace_loc, (image.shape))
+kspace_gridded_loc = convert_mask_to_locations(np.fft.fftshift(mask_gridded))
+fourier_op_gridded = FFT2(samples=kspace_gridded_loc, shape=image.shape)
+kspace_gridded_obs = fourier_op_gridded.op(image.data)
+image_rec0 = pysap.Image(data=fourier_op_gridded.adj_op(kspace_gridded_obs))
 image_rec0.show()
-
-kspace_gridded_loc = convert_mask_to_locations(convert_locations_to_mask(kspace_loc, (512,512)))
-fourier_op_gridded = NFFT(samples=kspace_gridded_loc, shape=image.shape)
-image_rec1 = pysap.Image(data=fourier_op_gridded.adj_op(fourier_op_gridded.op(image.data)))
-image_rec1.show()
+base_ssim = ssim(image_rec0, image)
+print('The Base SSIM is : ' + str(base_ssim))
 
 #############################################################################
 # FISTA optimization
@@ -75,24 +75,25 @@ gradient_op, linear_op, prox_op, cost_op = generate_operators(
     data=kspace_obs,
     wavelet_name="UndecimatedBiOrthogonalTransform",
     samples=kspace_loc,
-    mu=1e-5,
+    mu=6.45e-07,
     nb_scales=4,
     non_cartesian=True,
     uniform_data_shape=image.shape,
     gradient_space="synthesis")
 
 # Start the FISTA reconstruction
-max_iter = 20
+max_iter = 200
 x_final, transform, costs, metrics = sparse_rec_fista(
     gradient_op=gradient_op,
     linear_op=linear_op,
     prox_op=prox_op,
     cost_op=cost_op,
-    lambda_init=1,
+    lambda_init=1.0,
     max_nb_of_iter=max_iter,
     atol=1e-4,
     verbose=1)
 image_rec = pysap.Image(data=np.abs(x_final))
 image_rec.show()
-cost_rec = pysap.Image(data=costs)
-cost_rec.show()
+recon_ssim = ssim(image_rec, image)
+print('The Reconstruction SSIM is : ' + str(recon_ssim))
+
