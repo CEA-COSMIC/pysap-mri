dc9fd26111c131fe320bb3ae86093819b567a26c
chaithyagr <chaithyagr@gmail.com>
2019-10-04 16:08:09 +0200

    Add POGM test

--- mri/test/test_optimizer.py
+++ mri/test/test_optimizer.py
@@ -175,6 +175,46 @@ class TestOptimizer(unittest.TestCase):
                     np.testing.assert_allclose(x_final, np.fft.ifftshift(
                         fourier_0.adj_op(data_0)))
 
+    def test_reconstruction_pogm_nfft2(self):
+        """ Test reconstruction with POGM.
+        """
+        print("Process test NFFT2 FISTA::")
+        for image in self.images:
+            fourier = NFFT(samples=convert_mask_to_locations(
+                                            self.mask),
+                           shape=image.shape)
+            data = fourier.op(image.data)
+            print("Process test with image '{0}'...".format(
+                image.metadata["path"]))
+            for nb_scale in self.nb_scales:
+                print("- Number of scales: {0}".format(nb_scale))
+                for name in self.decimated_wavelets:
+                    print("    Transform: {0}".format(name))
+                    gradient_op, linear_op, prox_op, cost_op = \
+                        generate_operators(
+                            data=data,
+                            wavelet_name=name,
+                            samples=convert_mask_to_locations(self.mask),
+                            mu=0,
+                            nb_scales=4,
+                            non_cartesian=True,
+                            uniform_data_shape=image.shape,
+                            gradient_space="synthesis")
+                    x_final, costs, _ = sparse_rec_fista(
+                        gradient_op=gradient_op,
+                        linear_op=linear_op,
+                        prox_op=prox_op,
+                        cost_op=cost_op,
+                        lambda_init=1.0,
+                        max_nb_of_iter=self.nb_iter,
+                        verbose=0)
+                    fourier_0 = FFT2(samples=convert_mask_to_locations(
+                                            fftshift(self.mask)),
+                                     shape=image.shape)
+                    data_0 = fourier_0.op(np.fft.fftshift(image.data))
+                    np.testing.assert_allclose(x_final, np.fft.ifftshift(
+                        fourier_0.adj_op(data_0)))
+
     def test_reconstruction_condat_vu_nfft2(self):
         """ Test all the registered transformations.
         """
@@ -224,6 +264,8 @@ class TestOptimizer(unittest.TestCase):
                     data_0 = fourier_0.op(np.fft.fftshift(image.data))
                     np.testing.assert_allclose(x_final, np.fft.ifftshift(
                         fourier_0.adj_op(data_0)))
+
+
 
 
 if __name__ == "__main__":
