b226736b1be4b402c137889e197daf99f286415c
chaithyagr <chaithyagr@gmail.com>
2019-10-18 17:39:09 +0200

    Fix pyNFFT installation

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ setup(
                       "progressbar2",
                       "joblib",
                       "psutil",
-                      "pynfft==1.3.2"],
-    dependency_links=['https://github.com/ghisvail/pyNFFT/tarball/master#egg=pyNFFT-2.0.0'],
+                      "pynfft@git+https://github.com/ghisvail/pyNFFT@master"],
+    dependency_links=['https://github.com/ghisvail/pyNFFT/tarball/master#egg=pynfft-1.3.0'],
     tests_require=['pytest>=5.0.1', 'pytest-cov>=2.7.1', 'pytest-pep8'],
     platforms="OS Independent"
 )
