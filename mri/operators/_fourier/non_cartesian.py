Move Gradient Operators and fix PEP8

mri/operators/_fourier/non_cartesian.py
@@ -8,7 +8,7 @@
 ##########################################################################
 
 """
-Fourier operators for cartesian and non-cartesian space.
+Fourier operators for non-cartesian space.
 """
 
 # System import
@@ -16,9 +16,8 @@ import warnings
 import numpy as np
 
 # Package import
-from .utils import convert_locations_to_mask, normalize_frequency_locations, \
-    get_stacks_fourier
-from modopt.interface.errors import warn
+from .._base import OperatorBase
+from .utils import normalize_frequency_locations, get_stacks_fourier
 
 # Third party import
 try:
@@ -33,139 +32,6 @@ except Exception:
     warnings.warn("pynufft python package has not been found. If needed use "
                   "the master release. Till then you cannot use NUFFT on GPU")
     pass
-
-
-class FourierBase(object):
-    """ Base Fourier transform operator class.
-    """
-    def op(self, img):
-        """ This method calculates Fourier transform.
-
-        Parameters
-        ----------
-        img: np.ndarray
-            input image as array.
-
-        Returns
-        -------
-        result: np.ndarray
-            Fourier transform of the image.
-        """
-        raise NotImplementedError("'op' is an abstract method.")
-
-    def adj_op(self, x):
-        """ This method calculates inverse Fourier transform of real or complex
-        sequence.
-
-        Parameters
-        ----------
-        x: np.ndarray
-            input Fourier data array.
-
-        Returns
-        -------
-        results: np.ndarray
-            inverse discrete Fourier transform.
-        """
-        raise NotImplementedError("'adj_op' is an abstract method.")
-
-
-class FFT(FourierBase):
-    """ Standard unitary ND Fast Fourrier Transform class.
-    The FFT will be normalized in a symmetric way
-
-    Attributes
-    ----------
-    samples: np.ndarray
-        the mask samples in the Fourier domain.
-    shape: tuple of int
-        shape of the image (not necessarly a square matrix).
-     n_coils: int, default 1
-            Number of coils used to acquire the signal in case of multiarray
-            receiver coils acquisition. If n_coils > 1, data shape must be
-            [n_coils, Nx, Ny, NZ]
-    """
-    def __init__(self, samples, shape, n_coils=1):
-        """ Initilize the 'FFT' class.
-
-        Parameters
-        ----------
-        samples: np.ndarray
-            the mask samples in the Fourier domain.
-        shape: tuple of int
-            shape of the image (not necessarly a square matrix).
-         n_coils: int, default 1
-                Number of coils used to acquire the signal in case of
-                multiarray receiver coils acquisition. If n_coils > 1,
-                 data shape must be equal to [n_coils, Nx, Ny, NZ]
-        """
-        self.samples = samples
-        self.shape = shape
-        self._mask = convert_locations_to_mask(self.samples, self.shape)
-        if n_coils <= 0:
-            warn("The number of coils should be strictly positive")
-            n_coils = 1
-        self.n_coils = n_coils
-
-    def op(self, img):
-        """ This method calculates the masked Fourier transform of a ND image.
-
-        Parameters
-        ----------
-        img: np.ndarray
-            input ND array with the same shape as the mask. For multichannel
-            images the coils dimension is put first
-
-        Returns
-        -------
-        x: np.ndarray
-            masked Fourier transform of the input image. For multichannel
-            images the coils dimension is put first
-        """
-        if self.n_coils == 1:
-            return self._mask * np.fft.ifftshift(np.fft.fftn(
-                                    np.fft.fftshift(img), norm="ortho"))
-        else:
-            if self.n_coils > 1 and self.n_coils != img.shape[0]:
-                raise ValueError("The number of coils parameter is not equal"
-                                 "to the actual number of coils, the data must"
-                                 "be reshaped as [n_coils, Nx, Ny, Nz]")
-            else:
-                # TODO: Use joblib for parallelization
-                return np.asarray([self._mask * np.fft.ifftshift(np.fft.fftn(
-                                    np.fft.fftshift(img[ch]), norm="ortho"))
-                                   for ch in range(self.n_coils)])
-
-    def adj_op(self, x):
-        """ This method calculates inverse masked Fourier transform of a ND
-        image.
-
-        Parameters
-        ----------
-        x: np.ndarray
-            masked Fourier transform data. For multichannel
-            images the coils dimension is put first
-
-        Returns
-        -------
-        img: np.ndarray
-            inverse ND discrete Fourier transform of the input coefficients.
-            For multichannel images the coils dimension is put first
-        """
-        if self.n_coils == 1:
-            return np.fft.fftshift(np.fft.ifftn(
-                        np.fft.ifftshift(self._mask * x), norm="ortho"))
-        else:
-            if self.n_coils > 1 and self.n_coils != x.shape[0]:
-                raise ValueError("The number of coils parameter is not equal"
-                                 "to the actual number of coils, the data must"
-                                 "be reshaped as [n_coils, Nx, Ny, Nz]")
-            else:
-                # TODO: Use joblib for parallelization
-                return np.asarray([np.fft.fftshift(np.fft.ifftn(
-                                        np.fft.ifftshift(self._mask * x[ch]),
-                                        norm="ortho"))
-                                   for ch in range(self.n_coils)])
 
 
 class NFFT:
@@ -390,12 +256,12 @@ class NUFFT(Singleton):
         if verbosity > 0:
             print('Creating the NUFFT object...')
         if self.platform == 'opencl':
-            warn('Attemping to use OpenCL plateform. Make sure to '
-                 'have  all the dependecies installed')
+            warnings.warn('Attemping to use OpenCL plateform. Make sure to '
+                          'have  all the dependecies installed')
             Singleton.__init__(self)
             if self.getNumInstances() > 1:
-                warn('You have created more than one NUFFT object. '
-                     'This could cause memory leaks')
+                warnings.warn('You have created more than one NUFFT object. '
+                              'This could cause memory leaks')
             self.nufftObj = NUFFT_hsa(API='ocl',
                                       platform_number=None,
                                       device_number=None,
@@ -410,13 +276,13 @@ class NUFFT(Singleton):
                                radix=None)
 
         elif self.platform == 'cuda':
-            warn('Attemping to use Cuda plateform. Make sure to '
-                 'have  all the dependecies installed and '
-                 'to create only one instance of NUFFT GPU')
+            warnings.warn('Attemping to use Cuda plateform. Make sure to '
+                          'have  all the dependecies installed and '
+                          'to create only one instance of NUFFT GPU')
             Singleton.__init__(self)
             if self.getNumInstances() > 1:
-                warn('You have created more than one NUFFT object. '
-                     'This could cause memory leaks')
+                warnings.warn('You have created more than one NUFFT object. '
+                              'This could cause memory leaks')
             self.nufftObj = NUFFT_hsa(API='cuda',
                                       platform_number=None,
                                       device_number=None,
@@ -511,8 +377,9 @@ class NUFFT(Singleton):
         return img * np.sqrt(np.prod(self.Kd))
 
 
-class NonCartesianFFT(FourierBase):
+class NonCartesianFFT(OperatorBase):
     """This class wraps around different implementation algorithms for NFFT"""
+
     def __init__(self, samples, shape, implementation='cpu', n_coils=1):
         """ Initialize the class.
 
@@ -576,7 +443,7 @@ class NonCartesianFFT(FourierBase):
         return self.implementation.adj_op(coeffs)
 
 
-class Stacked3DNFFT(FourierBase):
+class Stacked3DNFFT(OperatorBase):
     """"  3-D non uniform Fast Fourier Transform class,
     fast implementation for Stacked samples. Note that the kspace locations
     must be in the form of a stack along z, with same locations in

mri/operators/_gradient/_base.py
@@ -13,6 +13,7 @@ import numpy as np
 from modopt.math.matrix import PowerMethod
 from modopt.opt.gradient import GradBasic
 
+
 class GradBaseMRI(GradBasic):
     """ Gradient Analysis class for single channel
         Parameters
@@ -33,22 +34,23 @@ class GradBaseMRI(GradBasic):
         num_check_lips : int default 10
             Number of iterations to check if lipschitz constant is correct
     """
+
     def __init__(self, data, operator, trans_operator, shape,
                  max_iter_spec_rad=10, lipschitz_cst=None, num_check_lips=10):
-        super(GradBaseMRI).__init__(data, operator, trans_operator)
+        super(GradBaseMRI, self).__init__(data, operator, trans_operator)
         if lipschitz_cst is not None:
             self.spec_rad = lipschitz_cst
             self.inv_spec_rad = 1.0 / self.spec_rad
         else:
-            calc_lips = PowerMethod(self.trans_op_op, operator.shape,
-                            data_type=np.complex, auto_run=False)
+            calc_lips = PowerMethod(self.trans_op_op, shape,
+                                    data_type=np.complex, auto_run=False)
             calc_lips.get_spec_rad(extra_factor=1.1,
                                    max_iter=max_iter_spec_rad)
             self.spec_rad = calc_lips.spec_rad
             self.inv_spec_rad = calc_lips.inv_spec_rad
         if num_check_lips > 0:
             is_lips = check_lipschitz_cst(f=self.trans_op_op,
-                                          x_shape=operator.img_shape,
+                                          x_shape=shape,
                                           lipschitz_cst=self.spec_rad,
                                           max_nb_of_iter=num_check_lips)
             if not is_lips:

mri/operators/_gradient/gradient.py
@@ -11,40 +11,83 @@
 This module contains classes for defining algorithm operators and gradients.
 """
 
+# Package import
+from ._base import GradBaseMRI
 
 # Third party import
 import numpy as np
-from modopt.math.matrix import PowerMethod
-from modopt.opt.gradient import GradBasic
 
 
-class GradAnalysis2(GradBasic, PowerMethod):
-
-    def __init__(self, data, fourier_op, max_iter_spec_rad):
-
-        GradBasic.__init__(self, data, fourier_op.op, fourier_op.adj_op)
+class GradAnalysis(GradBaseMRI):
+    def __init__(self, data, fourier_op, **kwargs):
+        super(GradAnalysis, self).__init__(data, fourier_op.op,
+                                           fourier_op.adj_op,
+                                           fourier_op.shape,
+                                           **kwargs)
         self.fourier_op = fourier_op
-        PowerMethod.__init__(self, self.trans_op_op, self.fourier_op.shape,
-                             data_type=np.complex, auto_run=False)
-        self.get_spec_rad(extra_factor=1.1, max_iter=max_iter_spec_rad)
 
 
-class GradSynthesis2(GradBasic, PowerMethod):
-
-    def __init__(self, data, linear_op, fourier_op, max_iter_spec_rad):
-
-        GradBasic.__init__(self, data, self._op_method, self._trans_op_method)
+class GradSynthesis(GradBaseMRI):
+    def __init__(self, data, linear_op, fourier_op, **kwargs):
         self.fourier_op = fourier_op
         self.linear_op = linear_op
-        coef = linear_op.op(np.zeros(fourier_op.shape).astype(np.complex))
-        PowerMethod.__init__(self, self.trans_op_op, coef.shape,
-                             data_type=np.complex, auto_run=False)
-        self.get_spec_rad(extra_factor=1.1, max_iter=max_iter_spec_rad)
+        coef = linear_op.op(np.zeros(fourier_op.shape))
+        self.linear_op_coeffs_shape = coef.shape
+        super(GradSynthesis, self).__init__(data,
+                                            self._op_method,
+                                            self._trans_op_method,
+                                            self.linear_op_coeffs_shape,
+                                            **kwargs)
 
-    def _op_method(self, data, *args, **kwargs):
-
+    def _op_method(self, data):
         return self.fourier_op.op(self.linear_op.adj_op(data))
 
-    def _trans_op_method(self, data, *args, **kwargs):
-
+    def _trans_op_method(self, data):
         return self.linear_op.op(self.fourier_op.adj_op(data))
+
+
+class GradSelfCalibrationAnalysis(GradBaseMRI):
+    def __init__(self, data, fourier_op, Smaps, **kwargs):
+        super(GradSelfCalibrationAnalysis, self).__init__(
+            data,
+            self._op_method,
+            self._trans_op_method,
+            fourier_op.shape,
+            **kwargs)
+        self.Smaps = Smaps
+        self.fourier_op = fourier_op
+
+    def _op_method(self, data):
+        data_per_ch = np.asarray([data * self.Smaps[ch]
+                                  for ch in range(self.Smaps.shape[0])])
+        return self.fourier_op.op(data_per_ch)
+
+    def _trans_op_method(self, coeff):
+        data_per_ch = self.fourier_op.adj_op(coeff)
+        return np.sum(data_per_ch * np.conjugate(self.Smaps), axis=0)
+
+
+class GradSelfCalibrationSynthesis(GradBaseMRI):
+    def __init__(self, data, fourier_op, linear_op, Smaps, **kwargs):
+        self.Smaps = Smaps
+        self.fourier_op = fourier_op
+        self.linear_op = linear_op
+        coef = linear_op.op(np.zeros(fourier_op.shape))
+        self.linear_op_coeffs_shape = coef.shape
+        super(GradSelfCalibrationSynthesis, self).__init__(
+            data,
+            self._op_method,
+            self._trans_op_method,
+            self.linear_op_coeffs_shape,
+            **kwargs)
+
+    def _op_method(self, coeff):
+        image = self.linear_op.adj_op(coeff)
+        image_per_ch = np.asarray([image * self.Smaps[ch]
+                                   for ch in range(self.Smaps.shape[0])])
+        return self.fourier_op.op(image_per_ch)
+
+    def _trans_op_method(self, data):
+        data_per_ch = self.fourier_op.adj_op(data)
+        image_recon = np.sum(data_per_ch * np.conjugate(self.Smaps), axis=0)
+        return self.linear_op.op(image_recon)

mri/operators/_linear/wavelet.py
@@ -131,6 +131,7 @@ class WaveletN(object):
 class WaveletUD2(object):
     """The wavelet undecimated operator using pysap wrapper.
     """
+
     def __init__(self, wavelet_id=24, nb_scale=4, multichannel=False,
                  n_cpu=1, backend='threading', verbose=0):
         """Init function for Undecimated wavelet transform
@@ -249,11 +250,11 @@ class WaveletUD2(object):
             the reconstructed data.
         """
         data_real = filter_convolve(
-                np.squeeze(unflatten(coefs.real, coeffs_shape)),
-                self.transform, filter_rot=True)
+            np.squeeze(unflatten(coefs.real, coeffs_shape)),
+            self.transform, filter_rot=True)
         data_imag = filter_convolve(
-                np.squeeze(unflatten(coefs.imag, coeffs_shape)),
-                self.transform, filter_rot=True)
+            np.squeeze(unflatten(coefs.imag, coeffs_shape)),
+            self.transform, filter_rot=True)
         return data_real + 1j * data_imag
 
     def adj_op(self, coefs):