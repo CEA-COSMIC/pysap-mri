7254b2a742dbf69e5b637891ee5e12de238ba1df
chaithyagr <chaithyagr@gitlab.com>
2020-01-31 14:27:33 +0100

    Working Gridsearch with example

---
+++ examples/gridsearch_launch.py
@@ -0,0 +1,90 @@
+"""
+Neuroimaging cartesian reconstruction
+=====================================
+
+Author: Chaithya G R
+
+In this tutorial we will use the pysap-mri's launch grid helper function
+to carry out grid search. We will search for best regulatization weight
+reconstruction and best wavelet for reconstruction, while search for a
+space of 5 weight values.
+"""
+
+# Imports
+from mri.operators import FFT, WaveletN
+from mri.operators.utils import convert_mask_to_locations
+from mri.reconstructors import SingleChannelReconstructor
+from mri.scripts.gridsearch import launch_grid
+
+from pysap.data import get_sample_data
+
+from modopt.opt.proximity import SparseThreshold
+from modopt.opt.linear import Identity
+from modopt.math.metrics import ssim
+import numpy as np
+
+
+# Load MR data and obtain kspace
+image = get_sample_data('2d-mri')
+mask = get_sample_data("cartesian-mri-mask")
+kspace_loc = convert_mask_to_locations(mask.data)
+fourier_op = FFT(samples=kspace_loc, shape=image.shape)
+kspace_data = fourier_op.op(image.data)
+# Define the keyword dictionaries based on convention
+ref = image
+metrics = {'ssim': {'metric': ssim,
+                    'mapping': {'x_new': 'test', 'y_new': None},
+                    'cst_kwargs': {'ref': image, 'mask': None},
+                    'early_stopping': True,
+                    },
+           }
+fourier_kwargs = {
+    'init_class': FFT,
+    'args':
+        {
+            'samples': kspace_loc,
+            'shape': image.shape,
+        }
+}
+linear_kwargs = {
+    'init_class': WaveletN,
+    'args':
+        {
+            'wavelet_name': ['sym8', 'sym12'],
+            'nb_scale': [3, 4]
+        }
+}
+regularizer_kwargs = {
+    'init_class': SparseThreshold,
+    'args':
+        {
+            'linear': Identity(),
+            'weights': np.geomspace(1e-8, 1e-6, 5),
+        }
+}
+optimizer_kwargs = {
+    # Just following convention
+    'args':
+        {
+            'optimization_alg': 'fista',
+            'num_iterations': 20,
+            'metrics': metrics,
+        }
+}
+reconstructor_kwargs = {
+    'init_class': SingleChannelReconstructor,
+    'args':
+        {
+            'gradient_formulation': 'synthesis',
+        }
+}
+# Call the launch grid function and obtain results
+raw_results, test_cases, key_names = launch_grid(
+    kspace_data=kspace_data,
+    fourier_kwargs=fourier_kwargs,
+    linear_kwargs=linear_kwargs,
+    regularizer_kwargs=regularizer_kwargs,
+    optimizer_kwargs=optimizer_kwargs,
+    reconstructor_kwargs=reconstructor_kwargs,
+    n_jobs=2,
+)
--- mri/scripts/gridsearch.py
+++ mri/scripts/gridsearch.py
@@ -7,16 +7,38 @@
 # for details.
 ##########################################################################
 
-from mri.operators import WaveletN
-
-from modopt.opt.proximity import SparseThreshold
 import itertools
 
 from joblib import Parallel, delayed
+import numpy as np
 
 
-def reconstruct_case(case, key_names, init_classes,
-                     kspace_data, fourier_kwargs):
+def _reconstruct_case(case, key_names, init_classes,
+                      kspace_data, fourier_kwargs):
+    """Internal Function to carry out reconstruction for a
+    special case. This function pulls in appropriate keyword arguments
+    from input and declares appropriate Linear, Fourier and Regularizer
+    Operators. These operators later are used to create the image model
+    by defining the reconstructor. Then the reconstruction is carried and
+    results are returned
+
+    Parameters
+    ----------
+    case: list of parameters
+        keyword arguments for different operators for reconstruction
+    key_names: list of strings
+        names of keyword arguments specified for reconstruction in 'case'
+    init_classes: list of classes
+        classes for varoius operators. must be in format
+        [linear, regularizer, reconstructor]
+    kspace_data: np.ndarray
+        the kspace data for reconstruction
+    fourier_kwargs: dict
+        holds dictionary with init_class pointing to fourier class to be used
+        and args having keyword arguments for initialization
+        NOTE: We declare fourier operator inside this function to allow
+        parallel execution as NonCartesianFFT cannot be pickled.
+    """
     fourier_op = fourier_kwargs['init_class'](**fourier_kwargs['args'])
     linear_op = init_classes[0](
         **dict(zip(key_names[0], case[0])))
@@ -31,100 +53,82 @@ def reconstruct_case(case, key_names, init_classes,
     )
     raw_results = reconstructor.reconstruct(
         kspace_data=kspace_data,
-        **dict(zip(key_names[3], case[3]))
+        **dict(zip(key_names[3], case[3])),
     )
     return raw_results, case
 
 
-def gridsearch(linear_kwargs, regularizer_kwargs, reconstructor_kwargs,
-               optimizer_kwargs, n_jobs=1, **kwargs):
-    cross_product_list = list(itertools.product(
-        *tuple(linear_kwargs['args'].values()),
-        *tuple(regularizer_kwargs['args'].values()),
-        *tuple(reconstructor_kwargs['args'].values()),
-        *tuple(optimizer_kwargs['args'].values()),
-    ))
+def launch_grid(linear_kwargs, regularizer_kwargs, reconstructor_kwargs,
+                optimizer_kwargs, n_jobs=1, **kwargs):
+    """This function launches off reconstruction for a grid specified
+    through use of kwarg dictionaries.
+    Dictionary Convention
+    ---------------------
+    These dictionaries each defined to follow the convention:
+    Each dictionary has a key `init_class` that specifies the
+    initialization class for the operator (exception to
+    this is 'optimizer_kwargs'). Later we have key `args` that holds
+    all the input arguments that can be passed as a keyword dictionary.
+    Each value in this keyword dictionary ,ust be a list of all
+    values you want to search in gridsearch.
+
+    This function finds the search space of parameters and
+    sets up right parameters for '_reconstruct_case' function.
+    Please check the example code for more details.
+
+    Parameters
+    ----------
+    linear_kwargs: dict
+        dictionary for linear operator parameters
+    regularizer_kwargs: dict
+        dictionary for regularizer operator parameters
+    reconstructor_kwargs: dict
+        dictionary for reconstructor operator parameters
+    optimizer_kwargs: dict
+        dictionary for optimizer key word arguments
+    n_jobs: int, default 1
+        number of parallel jobs for execution
+    **kwargs: keyword arguments that are passes to reconstruction
+        these holds extra arguments that are passed to
+        '_reconstruct_case' function.
+        important arguments are 'ksapce_data' and 'fourier_kwargs'
+    """
+    # Convert non-list elements to list so that we can create
+    # search space
+    all_reformatted_kwargs = []
+    for specific_kwargs in [linear_kwargs, regularizer_kwargs,
+                            reconstructor_kwargs, optimizer_kwargs]:
+        for key, value in specific_kwargs['args'].items():
+            if not isinstance(value, list) and \
+                    not isinstance(value, np.ndarray):
+                specific_kwargs['args'][key] = [value]
+        all_reformatted_kwargs.append(specific_kwargs)
+    # Create Search space
+    cross_product_list = list(itertools.product(*tuple(sum(
+        [list(all_reformatted_kwargs[i]['args'].values())
+         for i in range(len(all_reformatted_kwargs))],
+        []
+    ))))
+    # Obtain Initialization classes
+    init_classes = [
+        all_reformatted_kwargs[i]['init_class']
+        for i in range(len(all_reformatted_kwargs)-1)
+    ]
+    # Obtain key names for all cases
     key_names = list([
-        list(linear_kwargs['args'].keys()),
-        list(regularizer_kwargs['args'].keys()),
-        list(reconstructor_kwargs['args'].keys()),
-        list(optimizer_kwargs['args'].keys()),
+        list(all_reformatted_kwargs[i]['args'].keys())
+        for i in range(len(all_reformatted_kwargs))
     ])
     reshaped_cross_product = []
     for i in range(len(cross_product_list)):
         iterator = iter(cross_product_list[i])
-        reshaped_cross_product.append([[next(iter(iterator)) for _ in sublist] for sublist in key_names])
-    init_classes = [
-        linear_kwargs['init_class'],
-        regularizer_kwargs['init_class'],
-        reconstructor_kwargs['init_class'],
-    ]
+        reshaped_cross_product.append(
+            [[next(iter(iterator))
+              for _ in sublist] for sublist in key_names]
+        )
     results, test_cases = zip(*Parallel(n_jobs=n_jobs)(
-        delayed(reconstruct_case)
+        delayed(_reconstruct_case)
         (reshaped_cross_product[i], key_names, init_classes, **kwargs)
         for i in range(len(cross_product_list))
     ))
     return results, test_cases, key_names
-
-
-from mri.operators import FFT, WaveletN, NonCartesianFFT
-from mri.operators.utils import convert_mask_to_locations
-from mri.reconstructors import SingleChannelReconstructor
-from pysap.data import get_sample_data
-from modopt.opt.proximity import SparseThreshold
-from modopt.opt.linear import Identity
-import numpy as np
-
-image = get_sample_data('2d-mri')
-mask = get_sample_data("cartesian-mri-mask")
-kspace_loc = convert_mask_to_locations(mask.data)
-fourier_op = NonCartesianFFT(samples=kspace_loc, shape=image.shape)
-kspace_data = fourier_op.op(image.data)
-fourier_kwargs = {
-    'init_class': NonCartesianFFT,
-    'args':
-        {
-            'samples': kspace_loc,
-            'shape': image.shape,
-        }
-}
-linear_kwargs = {
-    'init_class': WaveletN,
-    'args':
-        {
-            'wavelet_name': ['sym8', 'sym12'],
-            'nb_scale': [3, 4]
-        }
-}
-regularizer_kwargs = {
-    'init_class': SparseThreshold,
-    'args':
-        {
-            'linear': [Identity()],
-            'weights': np.geomspace(1e-8, 1e-6, 2),
-        }
-}
-optimizer_kwargs = {
-    # Just following convention
-    'args':
-        {
-            'optimization_alg': ['fista', 'pogm'],
-            'num_iterations': [10, 20],
-        }
-}
-reconstructor_kwargs = {
-    'init_class': SingleChannelReconstructor,
-    'args':
-        {
-            'gradient_formulation': ['synthesis'],
-        }
-}
-results = gridsearch(
-    kspace_data=kspace_data,
-    fourier_kwargs=fourier_kwargs,
-    linear_kwargs=linear_kwargs,
-    regularizer_kwargs=regularizer_kwargs,
-    optimizer_kwargs=optimizer_kwargs,
-    reconstructor_kwargs=reconstructor_kwargs,
-    n_jobs=1,
-)
