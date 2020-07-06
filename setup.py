c4167e2a84aeb97e6d9541613eda616810d0a81e
chaithyagr <chaithyagr@gitlab.com>
2020-07-06 14:17:40 +0200

    Version bump and point to right repositories

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ Jean-Luc Starck <jl.stark@cea.fr>
 Philippe Ciuciu <philippe.ciuciu@cea.fr>
 """
 # Write setup
-setup_requires = ["numpy", "cython", "pytest-runner", "scikit-image"]
+setup_requires = ["numpy>=1.16.4", "cython>=0.27.3"]
 
 pip_main(['install'] + setup_requires)
 
@@ -42,17 +42,19 @@ setup(
     classifiers="CLASSIFIERS",
     author=AUTHOR,
     author_email="XXX",
-    version="0.1.1",
+    version="0.2.0",
     url="https://github.com/CEA-COSMIC/pysap-mri",
     packages=find_packages(),
     setup_requires=setup_requires,
-    install_requires=["scikit-learn",
-                      "progressbar2",
-                      "joblib",
-                      "scipy<=1.3.3",
-                      "psutil",
-                      "pynfft@git+https://github.com/ghisvail/pyNFFT@master"],
+    install_requires=[
+        "scikit-learn>=0.19.1",
+        "progressbar2>=3.34.3",
+        "joblib",
+        "scipy>=1.3.0",
+        "pynfft",
+        "scikit-image",
+    ],
     dependency_links=['https://github.com/ghisvail/pyNFFT/tarball/master#egg=pynfft-1.3.0'],
-    tests_require=['pytest>=5.0.1', 'pytest-cov>=2.7.1', 'pytest-pep8'],
+    tests_require=['pytest>=5.0.1', 'pytest-cov>=2.7.1', 'pytest-pep8', 'pytest-runner'],
     platforms="OS Independent"
 )
