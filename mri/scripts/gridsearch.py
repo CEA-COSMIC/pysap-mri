5d8c193bee34ef537b0641e322d454723674d287
chaithyagr <chaithyagr@gitlab.com>
2021-01-12 17:43:47 +0100

    Fix test and add minute change for grid search

--- mri/scripts/gridsearch.py
+++ mri/scripts/gridsearch.py
@@ -261,6 +261,7 @@ def launch_grid(kspace_data, reconstructor_class, reconstructor_kwargs,
                 {
                     'samples': fourier_op.samples,
                     'shape': fourier_op.shape,
+                    'n_coils': fourier_op.n_coils,
                 }
         }
         fourier_op = None
