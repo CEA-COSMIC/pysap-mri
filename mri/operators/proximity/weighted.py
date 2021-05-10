99b819243cf08c9a06a181d977944c1d58d6e42a
chaithyagr <chaithyagr@gitlab.com>
2021-05-10 12:12:31 +0200

    Add weighted codes

--- mri/operators/proximity/weighted.py
+++ mri/operators/proximity/weighted.py
@@ -4,6 +4,7 @@ from modopt.opt.proximity import SparseThreshold
 
 
 class WeightedSparseThreshold(SparseThreshold):
+    """This is """
     def __init__(self, weights, coeffs_shape, weight_type='scale_based', **kwargs):
         self.coeffs_shape = coeffs_shape
         self.weight_type = weight_type
@@ -22,7 +23,7 @@ class WeightedSparseThreshold(SparseThreshold):
         weights_init = np.zeros(np.sum(np.prod(self.coeffs_shape, axis=-1)))
         start = 0
         if self.weight_type == 'scale_based':
-            if type(w) == float or type(w) == int or len(w) == 1 or w.size == 1:
+            if isinstance(w, (float, int, np.float64)):
                 base_weight = w
                 power = 1
             else:
@@ -30,10 +31,10 @@ class WeightedSparseThreshold(SparseThreshold):
             for i, scale_shape in enumerate(np.unique(self.coeffs_shape, axis=0)):
                 scale_size = np.prod(scale_shape)
                 stop = start + scale_size * np.sum(scale_shape == self.coeffs_shape)
-                weights_init[start:stop] = base_weight * (power ** (i-1))
+                weights_init[start:stop] = base_weight * (power ** i)
                 start = stop
         elif self.weight_type == 'custom':
-            if type(w) == float or type(w) == int or len(w) == 1 or w.size == 1:
+            if isinstance(w, (float, int, np.float64)):
                 w = w * np.ones(weights_init.shape[0])
             weights_init = w
         weights_init[:np.prod(self.coeffs_shape[0])] = 0
