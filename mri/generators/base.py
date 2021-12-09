f68548f5da1a52f16e99a0a6f2eb5287f5456620
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2021-12-09 15:58:15 +0100

    add test and fix bugs.
    Using `opt.iterate` does more harm than good. (idex value is reset, and
    access to metrics is compromised.)

--- mri/generators/base.py
+++ mri/generators/base.py
@@ -101,9 +101,12 @@ class KspaceGeneratorBase:
             opt.idx += 1
             opt._grad.obs_data = kspace
             opt._grad.fourier_op.mask = mask
-            opt.iterate(max_iter=1)
+            opt._update()
+            if opt.metrics and opt.metric_call_period is not None:
+                if opt.idx % opt.metric_call_period == 0 or opt.idx == (self._len - 1):
+                    opt._compute_metrics()
             if estimate_call_period is not None:
-                if opt.idx % estimate_call_period == 0:
+                if opt.idx % estimate_call_period == 0 or opt.idx == (self._len - 1):
                     x_new_list.append(opt.get_notify_observers_kwargs()["x_new"])
-        opt.retrieve_outpts()
+        opt.retrieve_outputs()
         return np.asarray(x_new_list)
