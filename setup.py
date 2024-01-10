245d231c032a84765d5fef5f2e0e7eedd2438ed3
Chaithya G R <chaithyagr@gmail.com>
2024-01-10 11:59:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "joblib>=1.0.0",
         "scipy>=1.3.0",
         "scikit-image>=0.17.0",
-        "mri-nufft",
+        "mri-nufft@git+https://github.com/mind-inria/mri-nufft",
         "finufft",
     ],
     tests_require=['pytest>=5.0.1', 'pytest-cov>=2.7.1', 'pytest-pep8', 'pytest-runner'],
