a58a71409887b881cc237c0901f53bbbef073c65
Chaithya G R <chaithyagr@gmail.com>
2024-04-11 11:04:41 +0200

    blacked

--- src/mri/cli/dc_adjoint.py
+++ src/mri/cli/dc_adjoint.py
@@ -18,22 +18,19 @@ traj_config = builds(
     read_trajectory,
     populate_full_signature=True,
     zen_exclude=["dwell_time"],
-    zen_partial=True
+    zen_partial=True,
 )
 
 fourier_op_config = builds(
     NonCartesianFFT,
     populate_full_signature=True,
     zen_partial=True,
-    zen_exclude=["n_coils"]
+    zen_exclude=["n_coils"],
 )
 fourier_store = store(group="fourier")
 fourier_store(fourier_op_config, name="cpu")
 fourier_store(
-    fourier_op_config,
-    implementation="gpuNUFFT",
-    density_comp="pipe",
-    name="gpu"
+    fourier_op_config, implementation="gpuNUFFT", density_comp="pipe", name="gpu"
 )
 
 
@@ -48,10 +45,10 @@ def recon(obs_file: str, traj_file: str, obs_reader, traj_reader, fourier):
     traj_file : str
         Path to the trajectory file.
     obs_reader : callable
-        A function that reads the observed data file and returns 
+        A function that reads the observed data file and returns
         the raw data and data header.
     traj_reader : callable
-        A function that reads the trajectory file and returns the trajectory 
+        A function that reads the trajectory file and returns the trajectory
         data and parameters.
     fourier : callable
         A function that performs Fourier transform operations.
@@ -68,29 +65,27 @@ def recon(obs_file: str, traj_file: str, obs_reader, traj_reader, fourier):
     else:
         log.warn("Trajectory name not found in data header, validation step not done")
     shots, traj_params = traj_reader(
-        traj_file,
-        dwell_time=DEFAULT_RASTER_TIME/data_header["oversampling_factor"]
+        traj_file, dwell_time=DEFAULT_RASTER_TIME / data_header["oversampling_factor"]
     )
     kspace_loc = shots.reshape(-1, traj_params["dimension"])
-    normalized_shifts =  (
-        np.array(data_header["shifts"]) / np.array(traj_params["FOV"]) 
-        * np.array(traj_params["img_size"]) / 1000
+    normalized_shifts = (
+        np.array(data_header["shifts"])
+        / np.array(traj_params["FOV"])
+        * np.array(traj_params["img_size"])
+        / 1000
     )
     log.debug(f"Normalized shifts: {normalized_shifts}")
     kspace_data = add_phase_to_kspace_with_shifts(
-        np.squeeze(raw_data),
-        kspace_loc,
-        normalized_shifts
+        np.squeeze(raw_data), kspace_loc, normalized_shifts
     )
     fourier_op = fourier(
-        kspace_loc,
-        traj_params["img_size"],
-        n_coils=data_header["n_coils"]
+        kspace_loc, traj_params["img_size"], n_coils=data_header["n_coils"]
     )
     per_ch_image = fourier_op.adj_op(kspace_data)
     combined_image = np.linalg.norm(per_ch_image, axis=-1)
     pkl.dump(combined_image, open("dc_adjoint.pkl", "wb"))
-    
+
+
 store(
     recon,
     obs_reader=raw_config,
@@ -99,7 +94,7 @@ store(
         "_self_",
         {"fourier": "gpu"},
     ],
-    name="dc_adjoint"
+    name="dc_adjoint",
 )
 
 store.add_to_hydra_store()
